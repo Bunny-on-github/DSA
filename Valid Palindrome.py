103d91685cb52d8470d87c09e88bacc973b108a2
Yuvraj Singh <62551380+Bunny-on-github@users.noreply.github.com>
2024-09-01 15:16:29 +0530

    Added Edge case

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,4 +1,5 @@
 s = "({[({{([()])}})]})" 
+#s = "]" #Added this edge case 
 
 pairs = {
     "]" : "[",
@@ -12,11 +13,8 @@ def main():
     for i in s:
         if i in pairs.values():
             Stack.append(i)
-        elif not Stack or i in pairs:
-            if pairs[i] == Stack[-1]:
-                Stack.pop()
-            else:
-                return False
+        elif not Stack or Stack.pop() != pairs[i]: 
+            return False
     return not Stack
 
 print(main())
